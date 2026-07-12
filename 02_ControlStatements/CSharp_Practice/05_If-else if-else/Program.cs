f62636b679c22699a989d829ee89612f85f874ad
Abhay <>
2026-07-12 13:35:15 +0530

    controlStatement Done

---
+++ 02_ControlStatements/CSharp_Practice/05_If-else if-else/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int marks = 75;
+
+        if (marks >= 90)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if (marks >= 70)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else if (marks >= 50)
+        {
+            Console.WriteLine("Grade C");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
+    }
+}
