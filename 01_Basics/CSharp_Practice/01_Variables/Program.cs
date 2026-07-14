7d7e4c740e0483d787567f9ababd408e08733763
Abhay <>
2026-07-15 00:03:49 +0530

    variable program added

--- 01_Basics/CSharp_Practice/01_Variables/Program.cs
+++ 01_Basics/CSharp_Practice/01_Variables/Program.cs
@@ -1,10 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int age = 25;
-//double price = 99.99;
-//string name = "Rahul";
-//bool isActive = true;
-//char grade = 'A';
-//var city = "Delhi";  // compiler khud type detect karta hai
+﻿
+using System;
+//Student info
 
-Console.WriteLine(age);
+
+class Program
+{
+ static void Main()
+  {
+//        string name = "Abhay";
+//        int age = 25;
+//        double weight = 65.5;
+//        bool isStudent = true;
+
+//        Console.WriteLine("Name: " + name);
+//        Console.WriteLine("Age: " + age);
+//        Console.WriteLine("Weight: " + weight);
+//        Console.WriteLine("Student: " + isStudent);
+
+
+          string fullName = "Abhay J Padhiyar";
+
+          int age = 25;
+
+          string city = "Vadodara";
+
+          string qualification = "MCA";
+
+          char grade = 'A';
+
+          double percentage = 7.76;
+
+          bool isFresher = true;
+
+
+
+          Console.WriteLine("Fullname: " + fullName);
+
+          Console.WriteLine("Age: " + age);
+
+          Console.WriteLine("City: " + city);
+
+          Console.WriteLine("Qualification: " + qualification);
+
+          Console.WriteLine("Grade: " + grade);
+
+          Console.WriteLine("Percentage: " +percentage);
+
+          Console.WriteLine("Fresher: " +isFresher);
+
+
+   }
+}
+
